ea8e301b60eb36f9ca2c07113a2f0f73abe13a92
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2023-08-21 17:14:22 +0800

    fix indents & mask should have pointer icon

--- components/Modal.vue
+++ components/Modal.vue
@@ -30,35 +30,41 @@ const closeModal = () => {
 
 <style lang="scss" scoped>
 .modal--mask {
-  position: fixed;
-  z-index: 9998;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, .5);
-  transition: opacity .3s ease;
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    transition: opacity 0.3s ease;
+    cursor: pointer;
 }
 
 .modal--container {
-  margin: 40px auto 0;
-  padding: 20px 30px;
-  background-color: var(--bg-secondary);
-  border-radius: 10px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-  transition: all .3s ease;
+    margin: 40px auto 0;
+    padding: 20px 30px;
+    background-color: var(--bg-secondary);
+    border-radius: 10px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    transition: all 0.3s ease;
 
     position: absolute;
     transform: translate(-50%, -50%);
     top: 50%;
     left: 50%;
+    cursor: auto;
 }
 
 @media (max-width: 425px) {
-  .modal--container { width: 90vw; }
+    .modal--container {
+        width: 90vw;
+    }
 }
 
 @media (min-width: 426px) {
-  .modal--container { width: 400px; }
+    .modal--container {
+        width: 400px;
+    }
 }
 </style>
