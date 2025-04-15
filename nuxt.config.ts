03b64965d58dd208fee247ae8c05693f9651c591
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2025-04-15 23:19:49 +0800

    chore: add compatibility date on nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,45 +1,54 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-        pageTransition: { name: 'slide-right' },
-        head: {
-            titleTemplate: '%s - GlitchDZN'
-        }
-    },
-    vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    additionalData: '@import "@/assets/css/_variables.scss";',
-                }
-            }
-        }
-    },
-    experimental: {
-        payloadExtraction: false
-    },
-    nitro: {
-        prerender: {
-            autoSubfolderIndex: false
-        }
-    },
-    css: [
-        '~/assets/css/main.scss',
-        'milligram',
-        '@fortawesome/fontawesome-svg-core/styles.css'
-    ],
-    modules: [
-        '@nuxtjs/google-fonts',
-        '@nuxtjs/color-mode',
-        '@nuxtjs/supabase'
-    ],
-    googleFonts: {
-        display: 'swap',
-        families: {
-            Karla: [400, 700]
-        }
-    },
-    supabase: {
-        redirect: false
-    }
+  app: {
+      pageTransition: { name: 'slide-right' },
+      head: {
+          titleTemplate: '%s - GlitchDZN'
+      }
+  },
+
+  vite: {
+      css: {
+          preprocessorOptions: {
+              scss: {
+                  additionalData: '@import "@/assets/css/_variables.scss";',
+              }
+          }
+      }
+  },
+
+  experimental: {
+      payloadExtraction: false
+  },
+
+  nitro: {
+      prerender: {
+          autoSubfolderIndex: false
+      }
+  },
+
+  css: [
+      '~/assets/css/main.scss',
+      'milligram',
+      '@fortawesome/fontawesome-svg-core/styles.css'
+  ],
+
+  modules: [
+      '@nuxtjs/google-fonts',
+      '@nuxtjs/color-mode',
+      '@nuxtjs/supabase'
+  ],
+
+  googleFonts: {
+      display: 'swap',
+      families: {
+          Karla: [400, 700]
+      }
+  },
+
+  supabase: {
+      redirect: false
+  },
+
+  compatibilityDate: '2025-04-15'
 })
